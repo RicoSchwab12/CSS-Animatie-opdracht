44609b1f8ba24a24c73f5d8ac7bb8ee0642818e3
Rico Schwab <ricoschwab5@icloud.com>
2017-11-30 10:25:32 +0100

    opdracht af

--- Website/js/animate.js
+++ Website/js/animate.js
@@ -1,13 +1,13 @@
-const knop = document.getElementById('knop');
-let vlak = document.querySelectorAll('.vlakje');
+const knop = document.getElementById('button');
+let vlak = document.querySelectorAll('.block');
 
 const schuif =  () => {
-    vlak[0].classList.toggle('vlakje--schuif-uit');
+    vlak[0].classList.toggle('block--slide-out');
     setTimeout(function(){
-        vlak[1].classList.toggle('vlakje--schuif-uit');
+        vlak[1].classList.toggle('block--slide-out');
     }, 100);
     setTimeout(function(){
-        vlak[2].classList.toggle('vlakje--schuif-uit');
+        vlak[2].classList.toggle('block--slide-out');
     }, 200);
 }
 
